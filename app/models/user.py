60985bea2be2240ed2c4aa659a4815777eaa19f0
Kaitru <kiryakrutyak@yandex.ru>
2024-12-20 17:42:14 +0300

    2 lesson

---
+++ app/models/user.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import Relationship
+from app.backend.db import Base
+from sqlalchemy import Column, Integer, String
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    firstname = Column(String)
+    lastname = Column(String)
+    age = Column(Integer)
+    slug = Column(String, unique=True, index=True)
+    tasks = Relationship("Task", back_populates="user")
